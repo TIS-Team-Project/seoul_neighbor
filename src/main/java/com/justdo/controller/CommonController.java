c9db4f25df4fc46f6f4007573a655e23a40655f8
yoonseongd <53684010+yoonseongd@users.noreply.github.com>
2020-07-17 16:23:07 +0900

    상세페이지 /board/read/1처럼 쿼리형(?bno=1)이 안보이게 함
    http://localhost:8181/board/read?bno=1
    에서
    http://localhost:8181/board/read/1
    로 바꿈

--- src/main/java/com/justdo/controller/CommonController.java
+++ src/main/java/com/justdo/controller/CommonController.java
@@ -1,5 +1,7 @@
 package com.justdo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -37,18 +39,20 @@ public class CommonController {
 	
 	
 	// 목록 페이지 이동 //////////////////////////
-	@GetMapping("list")
+	@GetMapping("board/list")
 	public String list() {
 		return "board/list";
 	};
 	// 목록 페이지 이동 //
 	
-	//bno로 상세페이지 부르기/////////////////////
-	@GetMapping("read")
-	public String read(@RequestParam("bno") int bno, Model model) {
-		BoardVO vo=service.read(bno);
-		model.addAttribute("board",vo);
-		return "board/read";
+	//bno로 상세페이지 부르기   ---이 주석의 오른쪽 설명란은 볼 필요 없음.         board/read 주소 슬래쉬 뒤에 붙는 애들은 이녀석 적용이란 의미 -> httpServletRequest request는 clinet가 주소창에 입력한 요청을 담은 객체로 request.getRequestURI는 클라이언트가 친 주소창이고, 그걸 잘라서 http://localhost:8181/board/read/1의 bno인 1만 따로 bno라는 변수에 저장하고, vo에 bno=1담아서 jsp에 어트리뷰트 속성으로 보내서 jsp는 그 데이터로 클라이언트에게 보여줌/////////////////////
+	@GetMapping("board/read/*")
+	public String read(Model model, HttpServletRequest request) {
+		
+		int bno =  Integer.parseInt(request.getRequestURI().substring(request.getRequestURI().lastIndexOf("/")+1));
+		   BoardVO vo=service.read(bno);
+		      model.addAttribute("board",vo);
+		      return "board/read";
 	}
 	//bno로 상세페이지 부르기  
 	
